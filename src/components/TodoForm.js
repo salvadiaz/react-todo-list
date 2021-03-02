424e3393c2f0981d27ea412fa2dcf48180cf6135
salvadiaz <salvadiazpesci@gmail.com>
2021-03-01 21:15:15 -0300

    finished no-persistence app

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 export default function TodoForm(props) {
     const [input, setInput] = useState('');
+
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    })
 
     const handleChange = e =>{
         setInput(e.target.value);
@@ -10,22 +16,41 @@ export default function TodoForm(props) {
     const handleSubmit = e =>{
         e.preventDefault();
 
-        props.onSubmit({
-            id: Math.floor(Math.random() * 1000),
-            text: input
-        })
+         props.onSubmit({
+             id: Math.floor(Math.random() * 1000),
+             text: input
+         });
+        setInput('');
     }
 
     return (
         <form className="todo-form" onSubmit={handleSubmit}>
+            {props.edit ? (
+            <>
+            <input 
+            type="text" 
+            placeholder="Update item" 
+            value={input}
+            name="text" 
+            onChange={handleChange}
+            ref={inputRef}
+            className="todo-input edit" />
+            <button className="todo-button edit">Update</button>
+            </>
+            ) :
+            (
+            <>
             <input 
             type="text" 
             placeholder="Add a todo" 
             value={input}
             name="text" 
             onChange={handleChange}
+            ref={inputRef}
             className="todo-input" />
             <button className="todo-button">Add todo</button>
+            </>
+            )}
         </form>
     )
 }
